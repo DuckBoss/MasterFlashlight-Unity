47a40362f69f1a0ad47f6527561fae1e28a928e1
Jason <DuckBoss@users.noreply.github.com>
2017-12-24 15:21:32 -0500

    Add files via upload

--- JJFS/Modifiers/ModifierScripts/BatteryModifier.cs
+++ JJFS/Modifiers/ModifierScripts/BatteryModifier.cs
@@ -7,5 +7,11 @@ public class BatteryModifier : ScriptableObject {
     [Header("Battery Properties")]
     public float batteryCapacity;
     public float batteryDrainRate;
-    
+
+    //This determines at what ratio of battery life left that the light starts fading it's intensity.
+    //Formula: batteryCapacity/lightFadeRatio = Percent To Start Fading.
+    //Example: (100 batteryCapacity)/(4 lightFadeRatio) = light starts fading at 25% of battery capacity
+    [Tooltip("batteryCapacity/lightFadeRatio = Percent To Start Fading Flashlight.")]
+    public float lightFadeRatio;
+
 }
